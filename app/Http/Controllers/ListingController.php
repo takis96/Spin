73381ec1c64f4528e4a8a265eca63d939c3527f7
takis96 <nikolopoulospanagiotis96@gmail.com>
2023-02-06 19:31:26 +0200

    blade and web route for the search form

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,33 +8,33 @@ use Illuminate\Support\Facades\Log;
 class ListingController extends Controller
 {
     public function search(Request $request)
-{
-    $listings = Listing::query();
+    {
+        $listings = Listing::query();
 
-    if ($request->location) {
-        $listings->whereIn('location', $request->location);
+        if ($request->location) {
+            $listings->whereIn('location', $request->location);
+        }
+
+        if ($request->price) {
+            $listings->whereBetween('price', [$request->price['min'], $request->price['max']]);
+        }
+
+        if ($request->square_meters) {
+            $listings->whereBetween('square_meters', [$request->square_meters['min'], $request->square_meters['max']]);
+        }
+
+        if ($request->availability) {
+            $listings->whereIn('availability', $request->availability);
+        }
+
+        if ($request->type) {
+            $listings->whereIn('type', $request->type);
+        }
+
+        return $listings->get();
+
+        // Log the performed search
+        Log::channel('searches')->info("Search performed with parameters: " . json_encode($request->all()));
+
     }
-
-    if ($request->price) {
-        $listings->whereBetween('price', [$request->price['min'], $request->price['max']]);
-    }
-
-    if ($request->square_meters) {
-        $listings->whereBetween('square_meters', [$request->square_meters['min'], $request->square_meters['max']]);
-    }
-
-    if ($request->availability) {
-        $listings->whereIn('availability', $request->availability);
-    }
-
-    if ($request->type) {
-        $listings->whereIn('type', $request->type);
-    }
-
-    return $listings->get();
-
-    // Log the performed search
-    Log::channel('searches')->info("Search performed with parameters: " . json_encode($request->all()));
-
-}
 }
