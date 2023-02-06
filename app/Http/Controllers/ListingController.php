e0fddfd8b8f208fa167fcfe6a8b6e41ae69b9c4b
takis96 <nikolopoulospanagiotis96@gmail.com>
2023-02-06 20:12:01 +0200

    validation, testing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,37 +4,59 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class ListingController extends Controller
 {
     public function search(Request $request)
     {
-        $listings = Listing::query();
+        try {
+            $validator = Validator::make($request->all(), [
+                'location' => 'required|array',
+                'price' => 'required|array',
+                'price.min' => 'integer|min:0',
+                'price.max' => 'integer|min:0',
+                'square_meters' => 'required|array',
+                'square_meters.min' => 'integer|min:20|max:10000',
+                'square_meters.max' => 'integer|min:20|max:10000',
+                'availability' => 'required|in:sale,rent',
+                'type' => 'required|array',
+                'type' => 'required|in:apartment,studio,loft,maisonette',
+            ]);
 
-        if ($request->location) {
-            $listings->whereIn('location', $request->location);
+            $listings = Listing::query();
+
+            if ($request->location) {
+                $listings->whereIn('location', $request->location);
+            }
+
+            if ($request->price) {
+                $listings->whereBetween('price', [$request->price['min'], $request->price['max']]);
+            }
+
+            if ($request->square_meters) {
+                $listings->whereBetween('square_meters', [$request->square_meters['min'], $request->square_meters['max']]);
+            }
+
+            if ($request->availability) {
+                $listings->whereIn('availability', $request->availability);
+            }
+
+            if ($request->type) {
+                $listings->whereIn('type', $request->type);
+            }
+
+            $results = $listings->get();
+
+            // Log the performed search
+            Log::channel('searches')->info("Search performed with parameters: " . json_encode($request->all()));
+
+            return $results;
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
         }
-
-        if ($request->price) {
-            $listings->whereBetween('price', [$request->price['min'], $request->price['max']]);
-        }
-
-        if ($request->square_meters) {
-            $listings->whereBetween('square_meters', [$request->square_meters['min'], $request->square_meters['max']]);
-        }
-
-        if ($request->availability) {
-            $listings->whereIn('availability', $request->availability);
-        }
-
-        if ($request->type) {
-            $listings->whereIn('type', $request->type);
-        }
-
-        return $listings->get();
-
-        // Log the performed search
-        Log::channel('searches')->info("Search performed with parameters: " . json_encode($request->all()));
-
     }
+
 }
